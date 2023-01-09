c16669bd76b6ff6a90e833403cfadce62539ce31
WKFS-FRC\Ashok.Yadav <ashok.yadav@wolterskluwer.com>
2023-01-09 14:05:18 +0530

    create lighthouse js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,46 +1,16 @@
-module.exports ={
-    "ci": {
-      "collect": {
-        "url": [
-          "https://csaba.page/",
-          "https://csaba.page/about.html",
-          "https://csaba.page/blog/",
-          "https://csaba.page/blog/suunto-under-armour-integrations.html"
-        ],
-        "numberOfRuns": 1,
-        "staticDistDir": "./public",
-        "settings": {
-            "emulatedFormFactor": "desktop",
-            "disableStorageReset": "view",
-            "disableDeviceEmulation": true,
-            "disableCpuThrottling": true,
-            "disableNetworkThrottling": true,
-            "throttlingMethod": "provided"
-        }
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      staticDistDir: "./dist/ng-github-lighthouse-ci",
+    },
+    assert: {
+      assertions: {
+        "first-contentful-paint": ["warn", { maxNumericValue: 4000 }],
       },
-      "assert": {
-        "preset": "lighthouse:recommended",
-        "assertions": {
-          "first-contentful-paint": [
-            "warn",
-            {
-              "maxNumericValue": 2500,
-              "aggregationMethod": "optimistic"
-            }
-          ],
-          "interactive": [
-            "warn",
-            {
-              "maxNumericValue": 5000,
-              "aggregationMethod": "optimistic"
-            }
-          ],
-          "uses-long-cache-ttl": "off",
-          "uses-http2": "off"
-        }
-      },
-      "upload": {
-        "target": "temporary-public-storage"
-      }
-    }
-  }
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
