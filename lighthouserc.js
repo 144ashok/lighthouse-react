029b0781217213c2d0f1a73c4e738c571209683a
WKFS-FRC\Ashok.Yadav <ashok.yadav@wolterskluwer.com>
2023-01-09 15:57:53 +0530

    add css test 4

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,21 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      "url": [
-        "https://csaba.page/",
-        "https://csaba.page/about.html",
-        "https://csaba.page/blog/",
-        "https://csaba.page/blog/suunto-under-armour-integrations.html"
-      ],
       numberOfRuns: 3,
-      staticDistDir: "./public",
-      "settings": {
-        "emulatedFormFactor": "desktop",
-        "disableDeviceEmulation": true,
-        "disableCpuThrottling": true,
-        "disableNetworkThrottling": true,
-        "throttlingMethod": "provided"
-    }
+      staticDistDir: "./build",
     },
     assert: {
       assertions: {
